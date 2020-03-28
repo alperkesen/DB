47f87f9cb3dbd67c2f68116cabf47458a4c54052
alperkesen <kesen.alper@gmail.com>
2020-03-28 19:13:04 +0300

    Change folder naming in image dataset

--- data/image_dataset.py
+++ data/image_dataset.py
@@ -37,15 +37,15 @@ class ImageDataset(data.Dataset, Configurable):
             with open(self.data_list[i], 'r') as fid:
                 image_list = fid.readlines()
             if self.is_training:
-                image_path=[self.data_dir[i]+'/train_images/'+timg.strip() for timg in image_list]
-                gt_path=[self.data_dir[i]+'/train_gts/'+timg.strip()+'.txt' for timg in image_list]
+                image_path=[self.data_dir[i]+'/train_img/'+timg.strip() for timg in image_list]
+                gt_path=[self.data_dir[i]+'/train_gt/'+timg.strip()+'.txt' for timg in image_list]
             else:
-                image_path=[self.data_dir[i]+'/test_images/'+timg.strip() for timg in image_list]
+                image_path=[self.data_dir[i]+'/test_img/'+timg.strip() for timg in image_list]
                 print(self.data_dir[i])
                 if 'TD500' in self.data_list[i] or 'total_text' in self.data_list[i]:
-                    gt_path=[self.data_dir[i]+'/test_gts/'+timg.strip()+'.txt' for timg in image_list]
+                    gt_path=[self.data_dir[i]+'/test_gt/'+timg.strip()+'.txt' for timg in image_list]
                 else:
-                    gt_path=[self.data_dir[i]+'/test_gts/'+'gt_'+timg.strip().split('.')[0]+'.txt' for timg in image_list]
+                    gt_path=[self.data_dir[i]+'/test_gt/'+timg.strip().split('.')[0]+'.txt' for timg in image_list]
             self.image_paths += image_path
             self.gt_paths += gt_path
         self.num_samples = len(self.image_paths)
